0f0739336954c0d9b7b429bd5ad83199ab281ef3
Parsa Nazer <129178874+Greenstorm911@users.noreply.github.com>
2024-01-09 10:36:28 +0330

    fixed bugs of django_static_replacer.py use this version

--- django_static_replacer.py
+++ django_static_replacer.py
@@ -1,6 +1,6 @@
 import re
-
 # i would really apprecite a star or a fork =) <3
+
 def replace_pattern_in_text(input_file, output_file):
     pattern = r'(href|src)="assets/([^"]+)"'
     replacement = r'\1="{% static "/\2" %}"'
@@ -11,12 +11,13 @@ def replace_pattern_in_text(input_file, output_file):
             text_content = infile.read()
         modified_text = re.sub(pattern, replacement, text_content)
         with open(output_file, 'w') as outfile:
-            outfile.write(modified_text)
+            outfile.write("{% load static %}\n" + modified_text)
         print("my app is cooking like gordon ramsay", output_file)
 
 
-    except :
+    except:
         print("not working properly as it apears make a .txt file in the same directory with the name (input_text.txt)")
+
 
 if __name__ == "__main__":
     input_file_path = "input_text.txt"
